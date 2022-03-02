3a0230cb98e27e829332e7f40f929cb0fdf4e67d
Umair Jibran <umairjibran7@gmail.com>
2022-03-02 23:47:28 +0500

    Update fetch dates

--- assets/js/appointment.js
+++ assets/js/appointment.js
@@ -1,9 +1,18 @@
-var selectedDate = document.getElementById('date');
+const selectedDate = document.getElementById('date');
 selectedDate.addEventListener('change', function () {
   fetchTimes(selectedDate.value);
 });
 
-const fetchTimes = date => {
+document.getElementById('AvailableHours').hidden = true;
+document.getElementById('Medical Type').hidden = true;
+document.getElementById('source').hidden = true;
+document.getElementById('apptBtn').disabled = true;
+
+const fetchTimes = async date => {
+  document.getElementById('AvailableHours').hidden = true;
+  document.getElementById('Medical Type').hidden = true;
+  document.getElementById('source').hidden = true;
+  document.getElementById('apptBtn').disabled = false;
   let dateA = date.split('-');
   date =
     parseInt(dateA[0], 10) +
@@ -11,12 +20,11 @@ const fetchTimes = date => {
     parseInt(dateA[1], 10) +
     '/' +
     parseInt(dateA[2], 10);
-  fetch(
+  await fetch(
     `https://us-central1-drive-safe-medicals-26e5f.cloudfunctions.net/getFreeDates?date=${date}`
   )
     .then(response => response.json())
     .then(data => {
-        console.log({data, l: data.length})
       const availableHours = document.getElementById('AvailableHours');
       availableHours.innerHTML = '';
       if (date.length > 0) {
@@ -26,11 +34,15 @@ const fetchTimes = date => {
           option.innerHTML = hour;
           availableHours.appendChild(option);
         });
+        document.getElementById('AvailableHours').hidden = false;
+        document.getElementById('Medical Type').hidden = false;
+        document.getElementById('source').hidden = false;
+        document.getElementById('apptBtn').disabled = true;
       } else {
         const option = document.createElement('option');
         option.innerHTML = 'Select a date';
         option.value = '';
-        availableHours.appendChild(option);
+        availableHours.firstChild = option;
       }
     });
 };
