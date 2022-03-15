e258a3c12384fe31304d32ed2feea9d5f30e5862
yasinicl <yasin_icl@yahoo.com>
2022-03-16 03:48:37 +0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -208,21 +208,5 @@
       }
     }
   });
-  $('.testi3').owlCarousel({
-    loop: true,
-    margin: 30,
-    nav: false,
-    dots: true,
-    autoplay: true,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false
-      },
-      1024: {
-        items: 3
-      }
-    }
-  })
+  
 })()
